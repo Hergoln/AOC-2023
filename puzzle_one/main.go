979b17b297de7a3de3ffa129e0722ac91f7d3b2b
Hergoln <juliusz.malka@o2.pl>
2023-12-02 18:33:11 +0100

    refactored initial solution to be more generic

--- puzzle_one/main.go
+++ puzzle_one/main.go
@@ -14,35 +14,38 @@ func checkError(err error) {
 	}
 }
 
-func summarizeFile_1stPuzzle(fileName string) int {
+type handlerFunc func(string) int
+
+func lineHandler_1stPuzzle(line string) int {
+	firstNumber := -1
+	lastNumber := -1
+	for _, c := range line {
+		if unicode.IsDigit(c) {
+			if firstNumber < 0 {
+				firstNumber = int(c)
+			}
+			lastNumber = int(c)
+		}
+	}
+
+	intVar, _ := strconv.Atoi(string(firstNumber) + string(lastNumber))
+	return intVar
+}
+
+func summarizeFile(fileName string, handler handlerFunc) int {
 	file, err := os.Open(fileName)
 	checkError(err)
 	stream := bufio.NewScanner(file)
 	sum := 0
 
 	for stream.Scan() {
-		firstNumber := -1
-		lastNumber := -1
-		text := stream.Text()
-		for _, c := range text {
-			if unicode.IsDigit(c) {
-				if firstNumber < 0 {
-					firstNumber = int(c)
-				}
-				lastNumber = int(c)
-			}
-		}
-
-		intVar, _ := strconv.Atoi(string(firstNumber) + string(lastNumber))
-		sum = sum + intVar
+		sum = sum + lineHandler_1stPuzzle(stream.Text())
 	}
 	checkError(stream.Err())
 	return sum
 }
 
-func
-
 func main() {
-	sum := summarizeFile_1stPuzzle("puzzle_1st_part_input")
+	sum := summarizeFile("puzzle_1st_part_input", lineHandler_1stPuzzle)
 	fmt.Println("Summarized value is: ", sum)
 }
